6444fd52b417d3faea70de63699210666f88698e
Claudio Cambra <developer@claudiocambra.com>
2024-04-20 01:39:09 +0800

    Add ability to take URLRequest in WebViewRepresentable

--- NextSync/Login/WebViewRepresentable.swift
+++ NextSync/Login/WebViewRepresentable.swift
@@ -18,15 +18,18 @@ typealias InternalViewRepresentable = NSViewRepresentable
 #if os(iOS) || os(macOS)
 public struct WebViewRepresentable: InternalViewRepresentable {
     private let url: URL?
+    private let request: URLRequest?
     private let configuration: WKWebViewConfiguration
     private let setup: (WKWebView) -> Void
 
     public init(
         url: URL? = nil,
+        request: URLRequest? = nil,
         configuration: WKWebViewConfiguration = WKWebViewConfiguration(),
         setup: @escaping (WKWebView) -> Void
     ) {
         self.url = url
+        self.request = request
         self.configuration = configuration
         self.setup = setup
     }
@@ -50,13 +53,17 @@ public struct WebViewRepresentable: InternalViewRepresentable {
     func makeView() -> WKWebView {
         let view = WKWebView(frame: .null, configuration: configuration)
         setup(view)
-        tryLoad(url, into: view)
+        tryLoad(into: view)
         return view
     }
 
-    func tryLoad(_ url: URL?, into view: WKWebView) {
-        guard let url = url else { return }
-        view.load(URLRequest(url: url))
+    private func tryLoad(into view: WKWebView) {
+        guard url != nil || request != nil else { return }
+        if let request {
+            view.load(request)
+        } else if let url {
+            view.load(URLRequest(url: url))
+        }
     }
 }
 #endif
