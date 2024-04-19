02879253c634cf9ff6cad7c09f77e752e80d14af
Claudio Cambra <developer@claudiocambra.com>
2024-04-20 00:30:37 +0800

    Show LoginView in ContentView

--- NextSync/ContentView.swift
+++ NextSync/ContentView.swift
@@ -5,17 +5,24 @@
 //  Created by Claudio Cambra on 19/4/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Query var accounts: [AccountModel]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if accounts.isEmpty {
+            LoginView()
+        } else {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
