dabc0ccf6ea994cddcde1c7c4c78110ba961c14b
Claudio Cambra <developer@claudiocambra.com>
2024-04-19 22:10:44 +0800

    Add starter files

---
+++ NextSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextSync
+//
+//  Created by Claudio Cambra on 19/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextSync/NextSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextSyncApp.swift
+//  NextSync
+//
+//  Created by Claudio Cambra on 19/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NextSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
