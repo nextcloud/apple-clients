41c19f5ac2b56f8beff85b6cdbd94557ce9b9e3d
Claudio Cambra <developer@claudiocambra.com>
2024-04-20 19:10:49 +0800

    Add an AccountPicker view

---
+++ NextSync/Account/AccountPicker.swift
@@ -0,0 +1,25 @@
+//
+//  AccountPicker.swift
+//  NextSync
+//
+//  Created by Claudio Cambra on 20/4/24.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+struct AccountPicker: View {
+    @Query var accounts: [AccountModel] = []
+    @Binding var selection: AccountModel?
+
+    var body: some View {
+        Picker("Account", systemImage: "person", selection: $selection) {
+            ForEach(accounts) { account in
+                let name = account.displayname.isEmpty ? account.username : account.displayname
+                let display = "\(name) (\(account.serverUrl.host() ?? ""))"
+                Text(display).tag(Optional(account))
+            }
+        }
+    }
+}
