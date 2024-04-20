026a8c602a091cbb68b0c43ec685112d1707f1e6
Claudio Cambra <developer@claudiocambra.com>
2024-04-21 00:41:59 +0800

    Add NextSyncKit target

---
+++ NextSyncKit/NextSyncKit.h
@@ -0,0 +1,18 @@
+//
+//  NextSyncKit.h
+//  NextSyncKit
+//
+//  Created by Claudio Cambra on 21/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NextSyncKit.
+FOUNDATION_EXPORT double NextSyncKitVersionNumber;
+
+//! Project version string for NextSyncKit.
+FOUNDATION_EXPORT const unsigned char NextSyncKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NextSyncKit/PublicHeader.h>
+
+
