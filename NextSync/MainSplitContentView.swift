bf0fe8a5926ab4dffa8f3a8caa7ec83250407ea2
Claudio Cambra <developer@claudiocambra.com>
2024-04-20 19:11:33 +0800

    Set default account on launch

--- NextSync/MainSplitContentView.swift
+++ NextSync/MainSplitContentView.swift
@@ -6,15 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 import SwiftUI
 
 struct MainSplitContentView: View {
+    @Environment(\.modelContext) var modelContext
     @State var sidebarSelection: MainSidebar.Panel?
     @State var accountSelection: AccountModel?
 
     var body: some View {
         NavigationSplitView {
             MainSidebar(selection: $sidebarSelection, accountSelection: $accountSelection)
+                .onAppear {
+                    guard accountSelection == nil else { return }
+                    let fetchDescriptor = FetchDescriptor<AccountModel>()
+                    let accounts = try? modelContext.fetch(fetchDescriptor)
+                    accounts?.forEach { print($0.description) }
+                    accountSelection = accounts?.first
+                }
         } detail: {
             NavigationStack {
                 MainDetailView(selection: $sidebarSelection)
