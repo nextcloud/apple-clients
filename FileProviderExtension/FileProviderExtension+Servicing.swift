96fcb0ca77ccee86ac9470ef39ddf49d90f4ad75
Claudio Cambra <developer@claudiocambra.com>
2024-04-21 12:47:54 +0800

    Handle authentication in file provider extension servicing

--- FileProviderExtension/FileProviderExtension+Servicing.swift
+++ FileProviderExtension/FileProviderExtension+Servicing.swift
@@ -13,7 +13,37 @@ extension FileProviderExtension: NSFileProviderServicing {
         for itemIdentifier: NSFileProviderItemIdentifier,
         completionHandler: @escaping ([any NSFileProviderServiceSource]?, (any Error)?) -> Void
     ) -> Progress {
+        let appCommunicationService = AppCommunicationServiceSource(
+            domainIdentifier: domain.identifier,
+            authenticationHandler: { [weak self] serverUrl, username, password in
+                self?.authenticate(serverUrl: serverUrl, username: username, password: password)
+            }
+        )
         completionHandler([], nil)
         return Progress()
+    }
+
+    private func authenticate(serverUrl: URL, username: String, password: String) {
+        let nkc = ncKit.nkCommonInstance
+        let serverString = serverUrl.absoluteString
+        guard nkc.urlBase != serverString || nkc.user != username || nkc.password != password else {
+            return
+        }
+
+        ncKit.setup(user: username, userId: username, password: password, urlBase: serverString)
+
+        guard let manager = NSFileProviderManager(for: domain) else {
+            self.logger.error("Could not get manager for domain \(self.domain.rawIdentifier)")
+            return
+        }
+        let resolvedError = NSFileProviderError(.notAuthenticated)
+
+        Task {
+            do {
+                try await manager.signalErrorResolved(resolvedError)
+            } catch let error {
+                logger.error("Error signalling authentication error resolved: \(error)")
+            }
+        }
     }
 }
