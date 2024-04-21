4fa271b76ebc3e24e1d0edf505edcd5d203c0c0f
Claudio Cambra <developer@claudiocambra.com>
2024-04-21 19:34:28 +0800

    Add a basic web loading view

---
+++ NextSync/Web/WebLoadingView.swift
@@ -0,0 +1,35 @@
+//
+//  WebLoadingView.swift
+//  NextSync
+//
+//  Created by Claudio Cambra on 21/4/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct WebLoadingView<Content>: View where Content: View {
+    @Binding var isShowing: Bool
+    var content: () -> Content
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .center) {
+                self.content()
+                    .disabled(isShowing)
+                    .blur(radius: isShowing ? 3 : 0)
+
+                VStack {
+                    Text("Loading...")
+                        .bold()
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                }
+                .frame(width: geometry.size.width / 2, height: geometry.size.height / 5)
+                .background(Color.secondary.colorInvert())
+                .cornerRadius(20)
+                .opacity(isShowing ? 1 : 0)
+            }
+        }
+    }
+}
