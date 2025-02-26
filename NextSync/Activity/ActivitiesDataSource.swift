4313dba58fe20d288b363da8dee6500f0dbf0d33
Claudio Cambra <developer@claudiocambra.com>
2025-02-26 15:47:32 +0800

    Add method to fetch preview image for a given activity
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- NextSync/Activity/ActivitiesDataSource.swift
+++ NextSync/Activity/ActivitiesDataSource.swift
@@ -8,6 +8,7 @@
 import NextcloudKit
 import NextSyncKit
 import OSLog
+import SwiftyJSON
 import SwiftUI
 
 extension NKActivity: @retroactive Identifiable {}
@@ -17,6 +18,7 @@ class ActivitiesDataSource: ObservableObject {
     let activityLimit: Int
     let objectId: String?
     let objectType: String?
+    let previewSize: CGFloat
 
     @Published private(set) var activities = [NKActivity]()
     private var latestFetchedActivityId = 0
@@ -26,17 +28,85 @@ class ActivitiesDataSource: ObservableObject {
         account: AccountModel,
         activityLimit: Int = 50,
         objectId: String? = nil,
-        objectType: String? = nil
+        objectType: String? = nil,
+        previewSize: CGFloat = 32
     ) {
         self.account = account
         self.activityLimit = activityLimit
         self.objectId = objectId
         self.objectType = objectType
+        self.previewSize = previewSize
 
         account.addToNcKitSessions()
     }
 
-    func fetch() async -> NKError {
+    @discardableResult func preview(for activity: NKActivity) async -> Image? {
+        guard previews[activity.idActivity] == nil else {
+            logger.debug("Not fetching preview for \(activity.idActivity), already acquired")
+            return previews[activity.idActivity]
+        }
+
+        guard let previewsRaw = activity.previews,
+              let previewsJson = JSON(previewsRaw).array,
+              let previewJson = previewsJson.first,
+              let fileId = previewJson["fileId"].int
+        else {
+            logger.error(
+                """
+                Activity \(activity) did not have valid previews:
+                    \(String(data: activity.previews ?? Data(), encoding: .utf8) ?? "NULL")
+                """
+            )
+            return nil
+        }
+
+        return await withCheckedContinuation { continuation in
+            logger.debug("Getting preview for activity with fileId: \(fileId)")
+            NextcloudKit.shared.downloadPreview(
+                fileId: String(fileId),
+                width: Int(previewSize),
+                height: Int(previewSize),
+                account: account.ncKitAccount
+            ) { _, _, _, _, responseData, error in
+                guard error == .success else {
+                    self.logger.error(
+                        """
+                        Could not retrieve preview for activity with fileId \(fileId).
+                            Received error: \(error.errorDescription, privacy: .public)
+                        """
+                    )
+                    continuation.resume(returning: nil)
+                    return
+                }
+
+                guard let data = responseData?.data else {
+                    self.logger.error("Could not acquire image data.")
+                    continuation.resume(returning: nil)
+                    return
+                }
+
+                #if os(macOS)
+                let platformImage = NSImage(data: data)
+                #else
+                let platformimage = UIImage(data: data)
+                #endif
+                guard let platformImage else {
+                    self.logger.error("Could not create preview platform image.")
+                    continuation.resume(returning: nil)
+                    return
+                }
+
+                #if os(macOS)
+                continuation.resume(returning: Image(nsImage: platformImage))
+                #else
+                continuation.resume(returning: Image(uiImage: platformImage))
+                #endif
+                self.logger.info("Got preview for activity with fileId: \(fileId)")
+            }
+        }
+    }
+
+    @discardableResult func fetch() async -> NKError {
         logger.info("Retrieving activities for \(self.account.ncKitAccount, privacy: .public)")
         return await withCheckedContinuation { continuation in
             NextcloudKit.shared.getActivity(
