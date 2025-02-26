2389cf5d01d184076081836ccd3bf97194bb62b7
Claudio Cambra <developer@claudiocambra.com>
2025-02-26 13:54:01 +0800

    Implement activity fetching in data source
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- NextSync/Activity/ActivitiesDataSource.swift
+++ NextSync/Activity/ActivitiesDataSource.swift
@@ -7,11 +7,21 @@
 
 import NextcloudKit
 import NextSyncKit
+import OSLog
+import SwiftUI
+
+extension NKActivity: @retroactive Identifiable {}
+
 class ActivitiesDataSource: ObservableObject {
     let account: AccountModel
     let activityLimit: Int
     let objectId: String?
     let objectType: String?
+
+    @Published private(set) var activities = [NKActivity]()
+    private var latestFetchedActivityId = 0
+    private let logger = Logger(subsystem: Logger.subsystem, category: "ActivitiesDataSource")
+
     required init(
         account: AccountModel,
         activityLimit: Int = 50,
@@ -23,5 +33,35 @@ class ActivitiesDataSource: ObservableObject {
         self.objectId = objectId
         self.objectType = objectType
 
+        account.addToNcKitSessions()
+    }
+
+    func fetch() async -> NKError {
+        logger.info("Retrieving activities for \(self.account.ncKitAccount, privacy: .public)")
+        return await withCheckedContinuation { continuation in
+            NextcloudKit.shared.getActivity(
+                since: latestFetchedActivityId,
+                limit: activityLimit,
+                objectId: nil,
+                objectType: objectType,
+                previews: true,
+                account: account.ncKitAccount) {
+                    _ in // TODO: Task handler?
+                } completion: { account, activities, activityFirstKnown, activityLastGiven, _, error in
+                    defer { continuation.resume(returning: error) }
+                    guard error == .success, account == self.account.ncKitAccount else {
+                        self.logger.error(
+                            """
+                            Unable to retrieve activities, encountered error:
+                                \(error.errorDescription, privacy: .public)
+                            """
+                        )
+                        return
+                    }
+                    self.latestFetchedActivityId = max(activityFirstKnown, activityLastGiven)
+                    self.activities = activities
+                    self.logger.info("Retrieved \(activities.count) activities")
+                }
+        }
     }
 }
