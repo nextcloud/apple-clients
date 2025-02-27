03a91b7ef65083423e808874c39aeeb2278afd53
Claudio Cambra <developer@claudiocambra.com>
2025-02-27 17:19:25 +0800

    Add basic notifications data source
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ NextSyncKit/Data Sources/NotificationsDataSource.swift
@@ -0,0 +1,47 @@
+//
+//  NotificationsDataSource.swift
+//  NextSync
+//
+//  Created by Claudio Cambra on 27/2/25.
+//
+
+import NextcloudKit
+import OSLog
+import SwiftUI
+
+extension NKNotifications: @retroactive Identifiable {}
+
+@Observable
+public class NotificationsDataSource {
+    public let account: AccountModel
+    private(set) public var notifications = [NKNotifications]()
+    private let logger = Logger(subsystem: Logger.subsystem, category: "NotificationsDataSource")
+
+    required public init(account: AccountModel) {
+        self.account = account
+        account.addToNcKitSessions()
+    }
+
+    @discardableResult public func fetch() async -> NKError {
+        return await withCheckedContinuation { continuation in
+            NextcloudKit.shared.getNotifications(
+                account: account.ncKitAccount
+            ) { account, receivedNotifications, responseData, error in
+                defer { continuation.resume(returning: error) }
+                guard error == .success,
+                      account == self.account.ncKitAccount,
+                      let receivedNotifications
+                else {
+                    self.logger.error(
+                        """
+                        Unable to retrieve notifications, encountered error:
+                            \(error.errorDescription, privacy: .public)
+                        """
+                    )
+                    return
+                }
+                self.notifications = receivedNotifications
+            }
+        }
+    }
+}
