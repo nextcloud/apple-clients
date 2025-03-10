20b9555e64801955b178d43398d956d32b2355ba
Claudio Cambra <developer@claudiocambra.com>
2025-03-10 14:20:49 +0800

    Increase width of tray window
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- NextSync/MenuBarContentView.swift
+++ NextSync/MenuBarContentView.swift
@@ -65,7 +65,7 @@ struct MenuBarContentView: View {
             if let accountSelection {
                 ActivityList(account: accountSelection)
                     .listStyle(.plain)
-                    .frame(minHeight: 400)
+                    .frame(minWidth: 360, minHeight: 400)
                     .background(.regularMaterial)
                     .clipShape(RoundedRectangle(cornerRadius: contentBorderRadius))
                     .overlay(
